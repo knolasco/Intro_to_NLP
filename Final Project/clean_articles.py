430b915a006e7ff9a9899d3bd138cdef731179b0
Kevin Nolasco <kevnolasco95@gmail.com>
2022-05-08 10:41:12 -0700

    adding sentences

--- Final Project/clean_articles.py
+++ Final Project/clean_articles.py
@@ -16,7 +16,16 @@ def clean_text(text):
         text = re.sub(r'[0-9]+', 'number', text)
         text = text.lower().split()
     else:
-        text = ''
+        text = []
+    return text
+
+def clean_sents(text):
+    if type(text) == str:
+        text = text.replace('CNN', '')
+        text = re.sub(r'[0-9]+', 'number', text)
+        text = text.split('.')
+    else:
+        text = []
     return text
 
 class DataManager:
@@ -32,6 +41,9 @@ class DataManager:
     
     def clean_article_text(self):
         self.data['article_text_clean'] = self.data['Article text'].apply(clean_text)
+    
+    def article_sents(self):
+        self.data['article_text_clean'] = self.data['Article text'].apply(clean_sents)
 
     def filter_cols(self):
         self.data = self.data[['Headline', 'Category', 'Article text', 'Year', 'article_text_clean']]
